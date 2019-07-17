a9f308d0bfba5e8d3cf992c5c3cd2edec9baa44a
Douglas Berkley <douglasmberkley@gmail.com>
2019-07-17 09:01:16 +0900

    now with a base repo

---
+++ app/repositories/base_repository.rb
@@ -0,0 +1,36 @@
+class BaseRepository
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @elements = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file_path)
+  end
+
+  def all
+    @elements
+  end
+
+  def add(element)
+    element.id = @next_id
+    @next_id += 1
+    @elements << element
+    save_csv
+  end
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file_path, csv_options) do |row|
+      @elements << build_element(row)
+    end
+    @next_id = @elements.empty? ? 1 : @elements.last.id + 1
+  end
+
+  def save_csv
+    CSV.open(@csv_file_path, 'wb') do |csv|
+      csv << @elements.first.class.headers
+      @elements.each do |element| # INSTANCE
+        csv << element.build_row
+      end
+    end
+  end
+end
--- app/repositories/customer_repository.rb
+++ app/repositories/customer_repository.rb
@@ -1,41 +1,10 @@
 require 'csv'
+require_relative 'base_repository'
 
-class CustomerRepository
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
-    @customers = []
-    @next_id = 1
-    load_csv if File.exist?(@csv_file_path)
-  end
-
-  def all
-    @customers
-  end
-
-  def add(customer)
-    customer.id = @next_id
-    @next_id += 1
-    @customers << customer
-    save_csv
-  end
-
-  def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_file_path, csv_options) do |row|
-      # p row # acts like a hash!
-      row[:id] = row[:id].to_i
-      @customers << Customer.new(row)
-    end
-    @next_id = @customers.empty? ? 1 : @customers.last.id + 1
-  end
-
-  def save_csv
-    CSV.open(@csv_file_path, 'wb') do |csv|
-      csv << ['id', 'name', 'address']
-      @customers.each do |customer| # INSTANCE
-        csv << [customer.id, customer.name, customer.address]
-      end
-    end
+class CustomerRepository < BaseRepository
+  def build_element(row)
+    row[:id] = row[:id].to_i
+    Customer.new(row)
   end
 end
 
